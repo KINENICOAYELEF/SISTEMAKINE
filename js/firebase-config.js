259b676f77d586e432023ef1d28bfd846d6f74a8
KINENICOAYELEF <KINESIOLOGO.NICOLASAYELEF@GMAIL.COM>
2025-03-02 14:21:41 -0300

    Create firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,23 @@
+// Configuración de Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-storage.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-auth.js";
+
+// Tu configuración de Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyBYaNbZWHUS-Pvm49kmMtHw9LqqxUDySYA",
+  authDomain: "base-de-datos-poli.firebaseapp.com",
+  projectId: "base-de-datos-poli",
+  storageBucket: "base-de-datos-poli.firebasestorage.app",
+  messagingSenderId: "954754202697",
+  appId: "1:954754202697:web:e06171f6b0ade314259398"
+};
+
+// Inicializar Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const storage = getStorage(app);
+const auth = getAuth(app);
+
+export { app, db, storage, auth };
